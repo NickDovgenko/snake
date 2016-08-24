db4867f8a0f0ad67626a41d68791420d67dc7543
Nick <nick@MacBook-Pro-Nick.local>
2016-08-24 21:30:35 +0300

    lesson-12

---
+++ Snake/Snake/FoodCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Snake
+{
+	class FoodCreator
+	{
+		int mapWidht;
+		int mapHeight;
+		char sym;
+
+		Random random = new Random();
+
+		public FoodCreator(int mapWidht, int mapHeight, char sym)
+		{
+			this.mapWidht = Console.BufferWidth;
+			this.mapHeight = Console.BufferHeight;
+			this.sym = sym;
+		}
+
+		public Point CreateFood()
+		{
+			int x = random.Next(0, mapWidht);
+			int y = random.Next(0, mapHeight);
+			return new Point(x, y, sym);
+
+		}
+
+	}
+}
+
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,15 +24,32 @@ namespace Snake
 			Point p = new Point (1, 3, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(Console.BufferHeight, Console.BufferWidth, '$');
+			Point food = foodCreator.CreateFood();
+				food.Draw();
+
 			while (true)
 			{
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{ 
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+
 				if (Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey();
 					snake.handleKey(key.Key);
 				}
-				Thread.Sleep(100);
-				snake.Move();
+
+
 			}
 
 		}
